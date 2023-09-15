a5c946f7d1600270e92a73e580ea4b1750bdd4d9
sabari245 <sabari2004@hotmail.com>
2023-09-15 21:49:24 +0530

    optimized the api code; file reading still not working

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -4,10 +4,6 @@ import fs from 'fs';
 import path from 'path';
 import csv from 'csv-parser';
 
-const dashboard1 = `<iframe title="ALL CHANNEL DASHBOARD" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=e37360c8-956a-4fc4-a932-1384cc2c64ab&autoAuth=true&ctid=d2c967e6-4fc7-452c-9e79-f78c68ccf915" frameborder="0" allowFullScreen="true"></iframe>`
-const dashboard2 = `<iframe title="ADVICER - DAILY REPORT" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=611f0baf-1a6b-4d81-9500-abc3afd293cc&autoAuth=true&ctid=d2c967e6-4fc7-452c-9e79-f78c68ccf915" frameborder="0" allowFullScreen="true"></iframe>`
-const dashboard3 = ` <iframe title="Master daily report" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ae359a0e-529d-42c3-b5f3-de474f0efef0&autoAuth=true&ctid=d2c967e6-4fc7-452c-9e79-f78c68ccf915" frameborder="0" allowFullScreen="true"></iframe>`
-
 export async function POST(req: NextRequest) {
     const formData = await req.formData()
     const email = formData.get('email')
@@ -32,17 +28,11 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({
             message: "user found",
             user: userRecord,
-            dashboard: [
-                dashboard1,
-                dashboard2,
-                dashboard3   
-            ]
         })
     }else {
         return NextResponse.json({
             message: "user not found",
             user: {},
-            dashboard: []
         })
     }
 }
