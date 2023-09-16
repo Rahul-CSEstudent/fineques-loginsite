cdd27ceee781ecb9fdcebb80bcebd8ff34031781
sabari245 <sabari2004@hotmail.com>
2023-09-16 17:47:36 +0530

    added login check to all the dashboards; added auth0 to the project

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,22 @@
+'use client';
+
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useRouter } from "next/navigation";
+import {useEffect} from "react";
+
 const Dashboard: React.FC = () => {
 
+    const {user, error, isLoading } = useUser();
+    const router = useRouter();
+
+    useEffect(()=>{
+      if (!user){
+        router.replace("/")
+      }
+    }, [user])
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
     return (
       <div style={{ width: '100%', height: '100vh' }}>
       <iframe
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body className={inter.className}>{children}</body>
+      </UserProvider>
     </html>
   )
 }
