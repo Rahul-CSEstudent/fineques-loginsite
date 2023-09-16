05e0f2fddada391799546de1185256b27b1c1377
sabari245 <sabari2004@hotmail.com>
2023-09-16 17:46:58 +0530

    added login check to all the dashboards; added auth0 to the project

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -1,38 +1,40 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import fs from 'fs';
-import path from 'path';
-import csv from 'csv-parser';
+import { verbose } from 'sqlite3';
+import { hash } from 'bcrypt';
+const sqlite3 = verbose();
 
 export async function POST(req: NextRequest) {
+    const db = new sqlite3.Database('./users.db');
     const formData = await req.formData()
-    const email = formData.get('email')
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
 
-    const userDataPath = path.join(__dirname, 'UserData.csv');
-    // const userDataPath = "./UserData.csv";
+    const hashedPassword = await hash(password, 10);
 
-    const fileData = fs.readFileSync(userDataPath, 'utf-8');
-    const records:any = [];
-    const rows = fileData.split('\n');
-    rows.forEach((row:any) => {
-        const [empnumber, fullname, email] = row.split(',');
-        if (empnumber && fullname && email) {
-            records.push({ empnumber, fullname, email });
-        }
+    const getUserPromise = new Promise((resolve, reject) => {
+        db.serialize(() => {
+            db.get("SELECT employee_id, full_name, email FROM users WHERE email = ? AND password = ?", [email, hashedPassword], (err: any, row: any) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (row) {
+                        resolve(NextResponse.json(row));
+                    } else {
+                        resolve(NextResponse.json({
+                            message: "User not found"}));
+                    }
+                }
+            });
+        });
+    }).finally(() => {
+        db.close();
     });
-    
 
-    const userRecord = records.find((record:any) => record.email === email);
-    console.log(userRecord);
-    if (userRecord){
+    return await getUserPromise.catch(()=>{
         return NextResponse.json({
-            message: "user found",
-            user: userRecord,
+            message: "Server error"
         })
-    }else {
-        return NextResponse.json({
-            message: "user not found",
-            user: {},
-        })
-    }
+    })
+
 }
